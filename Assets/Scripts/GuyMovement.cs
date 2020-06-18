381c6e1425088f1b995fbf43e5ae22a90d790be7
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-06-18 22:57:55 +0200

    ADD: Character sprite & movement

---
+++ Assets/Scripts/GuyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuyMovement : MonoBehaviour
+{
+	//Config parameters
+	[Header("Movement values")]
+	[SerializeField] float moveSpeed;
+
+	//Cache
+	Rigidbody2D rb;
+	Animator animator;
+	Vector2 movement;
+
+	
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+
+	void Update()
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+
+		animator.SetFloat("horizontal", movement.x);
+		animator.SetFloat("vertical", movement.y);
+		animator.SetFloat("speed", movement.sqrMagnitude); //magnitude?
+	}
+
+	private void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+}
